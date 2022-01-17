873bcd3490e993bd2cef43375c4147d1dc06b031
Sagar Kumar <19gietcse003@gmail.com>
2022-01-17 10:27:46 +0530

    .

--- message.php
+++ message.php
@@ -8,13 +8,13 @@
 
     if (!empty($email) && !empty($message)) {
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {  // if user entered email is valid
-            $receiver = "sagar19003@gmail.com"; // email receiver mail id
+            $to = "sagar19003@gmail.com"; // email receiver mail id
             $subject = "From: $name <$email>"; // subject of the email
             // merging all user values inside body variable.
             $body = "Name: $name \n Email: $email \n Phone: $phone \n Website: $website \n Message: $message \n ";
             $sender = "From: $email";
             // mail is inbuilt php function to send mail
-            if (mail($receiver, $subject, $body, $sender)) {
+            if (mail($to, $subject, $body, $sender)) {
                 echo "Your message has been send successful ✔";
             } else {
                 echo "Sorry, failed to send your message!";
