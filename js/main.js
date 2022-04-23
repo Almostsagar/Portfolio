b0c9ef3a857bfb5f0f1c947e3497f82cd87427fb
Sagar Kumar <19gietcse003@gmail.com>
2022-04-23 20:29:10 +0530

    .added aos

--- js/main.js
+++ js/main.js
@@ -16,3 +16,14 @@ $(document).ready(function () {
 // link.href = "https://duckduckgo.com/?q=a+tag+inside+a+tag+html&ia=web";
 // link.target = '_blank';
 
+const scrollProgress = document.getElementById('scroll-progress');
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
+
+
